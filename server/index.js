5f2967139543b50bab64c6ab11a1e0b7bca33a4d
Moin Uddin <U_Moin75@yahoo.com>
2018-08-10 19:27:46 -0400

    install mongoose and connect to mongodb database through docker

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 
-
+//MiddleWare
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+//Mongodb Mongoose Connection
+if(process.env.NODE_ENV === 'development') {
+   mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongo:27017/admin`);
+}
 
 app.get('/yo', (req, res, next) => {
-    res.send({first: "kj" });
+    res.send({first: "whatever" });
 });
 
 app.listen(8080, console.log(
